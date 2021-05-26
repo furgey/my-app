e6c36c08286c0b9347aa91e2bb34a987e2853063
furgey <jfurgurson@gmail.com>
2021-05-26 17:37:27 -0400

    Initial commit

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+    return (
+      <body>
+        <h1>MyName</h1>
+      <p>Test</p>
+      <ol>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ol>
+      </body>
+    )
+  }
+
+  export default MyInfo
